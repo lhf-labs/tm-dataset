4fbcd0b9441d1dd9ab1d074b3a074d1f0f528ed1
Asier <asierguti96@gmail.com>
2021-12-04 14:57:16 +0100

    checkpoint

--- h_statistics.py
+++ h_statistics.py
@@ -13,12 +13,32 @@ def plot_by_year(data):
     years_counts = Counter(sorted([d['year'] for d in data]))
     years = list(years_counts.keys())
     counts = list(years_counts.values())
+    fig, ax = plt.subplots(1)
+    plt.bar(range(len(years)), counts)
+    ax.set_xticklabels(years, rotation=45)
+    ax.set_xticks(range(len(years)))
+    ax.set_ylabel("Count")
+    ax.set_xlabel("Years")
+    ax.set_title("Trademark count by year")
+    plt.tight_layout()
+    fig.savefig('./analysis/by_year.pdf')
+    fig.savefig('./analysis/by_year.svg')
 
 
 def plot_assign(data):
     ncodes_count = Counter(sorted([len(d['vienna_codes']) for d in data]))
     ncodes = list(ncodes_count.keys())
     counts = list(ncodes_count.values())
+    fig, ax = plt.subplots(1)
+    plt.bar(range(len(ncodes)), counts)
+    ax.set_xticklabels(ncodes, rotation=45)
+    ax.set_xticks(range(len(counts)))
+    ax.set_ylabel("Count")
+    ax.set_xlabel("Assignment")
+    ax.set_title("Vienna code assignment by trademark")
+    plt.tight_layout()
+    fig.savefig('./analysis/by_trademark.pdf')
+    fig.savefig('./analysis/by_trademark.svg')
 
 
 def get_base(data):
@@ -67,6 +87,7 @@ if __name__ == '__main__':
 
     plot_by_year(data)
     plot_assign(data)
+    exit(0)
     vc_obj = get_base(data)
     with open('../stats.json', 'w') as fout:
         json.dump(vc_obj, fout)
